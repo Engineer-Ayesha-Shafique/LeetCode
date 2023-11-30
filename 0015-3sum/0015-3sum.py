2067f485a0590a1003f225440c7c5761afcaba70
Ayesha Shafique <Shafiqr504@gmail.com>
2023-11-30 23:41:30 +0500

    Time: 835 ms (65.72%), Space: 16.6 MB (52.89%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        # Sort the array to easily skip duplicates
+        nums.sort()
+
+        # Initialize an empty list to store the result triplets
+        result = []
+
+        # Iterate through the array
+        for i in range(len(nums) - 2):
+            # Skip duplicates
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            # Set pointers for the remaining part of the array
+            left, right = i + 1, len(nums) - 1
+
+            # Check for triplets
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+
+                if total < 0:
+                    left += 1
+                elif total > 0:
+                    right -= 1
+                else:
+                    # Found a triplet
+                    result.append([nums[i], nums[left], nums[right]])
+
+                    # Skip duplicates
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+
+                    # Move pointers
+                    left += 1
+                    right -= 1
+
+        return result
