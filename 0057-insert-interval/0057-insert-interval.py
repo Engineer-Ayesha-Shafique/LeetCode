28f1da7f49e0e2076536ff1204f1662118496060
Ayesha Shafique <Shafiqr504@gmail.com>
2023-12-03 13:27:03 +0500

    Time: 49 ms (76.34%), Space: 16.8 MB (34.93%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -28,14 +28,4 @@ class Solution(object):
 
         return result
 
-# Example usage:
-intervals1 = [[1,3],[6,9]]
-newInterval1 = [2,5]
-solution = Solution()
-output1 = solution.insert(intervals1, newInterval1)
-print(output1)  # Output: [[1,5],[6,9]]
 
-intervals2 = [[1,2],[3,5],[6,7],[8,10],[12,16]]
-newInterval2 = [4,8]
-output2 = solution.insert(intervals2, newInterval2)
-print(output2)  # Output: [[1,2],[3,10],[12,16]]
