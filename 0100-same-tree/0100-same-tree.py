c6cf0593518e25d1cd38e76a43f0905ed3969570
Ayesha Shafique <Shafiqr504@gmail.com>
2023-12-03 13:50:27 +0500

    Time: 15 ms (46.98%), Space: 13.5 MB (16.91%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -5,6 +5,13 @@
 #         self.left = left
 #         self.right = right
 
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution(object):
     def isSameTree(self, p, q):
         """
@@ -12,15 +19,31 @@ class Solution(object):
         :type q: TreeNode
         :rtype: bool
         """
-        # Base case: Both nodes are None, considered the same
-        if not p and not q:
-            return True
-        # If one of the nodes is None and the other is not, they are different
-        if not p or not q:
-            return False
-        # If the values of the current nodes are different, the trees are different
-        if p.val != q.val:
-            return False
+        def preorder(node1, node2):
+            # Base case: If both nodes are None, they are equal
+            if not node1 and not node2:
+                return True
+            # If one of the nodes is None and the other is not, they are not equal
+            if not node1 or not node2:
+                return False
+            # Check if the values are equal, and recursively check left and right subtrees
+            return (node1.val == node2.val) and \
+                   preorder(node1.left, node2.left) and \
+                   preorder(node1.right, node2.right)
 
-        # Recursively check the left and right subtrees
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        # Start the preorder traversal from the roots of both trees
+        return preorder(p, q)
+
+# Example usage:
+# Construct the trees
+tree_p = TreeNode(1, TreeNode(2), TreeNode(3))
+tree_q = TreeNode(1, TreeNode(2), TreeNode(3))
+
+# Create an instance of the Solution class
+solution = Solution()
+
+# Check if the trees are the same
+result = solution.isSameTree(tree_p, tree_q)
+
+# Print the result
+print(result)
