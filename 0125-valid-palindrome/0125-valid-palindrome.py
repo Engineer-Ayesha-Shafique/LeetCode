43bfbdbf9fe8de4bd516e1119fa923f9fb2eadf5
Ayesha Shafique <Shafiqr504@gmail.com>
2023-12-09 00:48:55 +0500

    Time: 15 ms (98.94%), Space: 15.9 MB (14.46%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -11,9 +11,3 @@ class Solution(object):
 
         # Compare the original string with its reverse
         return cleaned_s == cleaned_s[::-1]
-
-# Example usage:
-solution = Solution()
-print(solution.isPalindrome("A man, a plan, a canal: Panama"))  # Output: True
-print(solution.isPalindrome("race a car"))                      # Output: False
-print(solution.isPalindrome(" "))                                # Output: True
