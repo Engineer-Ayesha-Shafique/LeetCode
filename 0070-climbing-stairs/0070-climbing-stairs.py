3fced59bfa9b15e581f185a6e89d9d063fbf14b9
Ayesha Shafique <Shafiqr504@gmail.com>
2023-12-13 19:28:52 +0500

    Time: 14 ms (49.57%), Space: 13.2 MB (44.32%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+
+        # Initialize an array to store the number of ways to reach each step
+        ways = [0] * n
+        ways[0] = 1
+        ways[1] = 2
+
+        # Calculate the number of ways for each step from 3 to n
+        for i in range(2, n):
+            ways[i] = ways[i - 1] + ways[i - 2]
+
+        return ways[n - 1]
