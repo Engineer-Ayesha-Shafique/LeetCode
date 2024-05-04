297ba497e4cc992bdecd28f634c55f5417b35d44
Ayesha Shafique <Shafiqr504@gmail.com>
2024-05-04 14:18:33 +0500

    Time: 36 ms (72.65%), Space: 16.6 MB (58.72%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -1,14 +1,8 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
-class Solution(object):
-    def mergeTwoLists(self, list1, list2):
+class Solution:
+    def mergeTwoLists(self, A: Optional[ListNode], B: Optional[ListNode]) -> Optional[ListNode]:
         """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
+        :type A: Optional[ListNode]
+        :type B: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
         # Create a dummy node to serve as the starting point of the merged list
@@ -17,23 +11,23 @@ class Solution(object):
         current = dummy
 
         # Iterate through both lists until one of them is exhausted
-        while list1 is not None and list2 is not None:
+        while A is not None and B is not None:
             # Compare values of the current nodes in both lists
-            if list1.val < list2.val:
-                current.next = list1
-                list1 = list1.next
+            if A.val < B.val:
+                current.next = A
+                A = A.next
             else:
-                current.next = list2
-                list2 = list2.next
+                current.next = B
+                B = B.next
 
             # Move the current pointer to the newly added node
             current = current.next
 
         # If one of the lists is not exhausted, append the remaining nodes
-        if list1 is not None:
-            current.next = list1
-        elif list2 is not None:
-            current.next = list2
+        if A is not None:
+            current.next = A
+        elif B is not None:
+            current.next = B
 
         # Return the merged list starting from the dummy node's next
         return dummy.next
